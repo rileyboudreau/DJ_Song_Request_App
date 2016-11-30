4c6dae4fd99f495fe347a91c56ddba655fdd9d11
RileyBoudreau <rileyboudreau97@gmail.com>
2016-11-30 10:41:11 -0400

    Push for Rob

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,6 +1,4 @@
 angular.module('dj_song_request').controller('dj_dashboardController', function($scope) {
 	
-	$scope.alertButton = function() {
-		alert("This is the DJ Dashboard page!");
-	};
+	$scope.requestsList = request.getrequests();
 });
